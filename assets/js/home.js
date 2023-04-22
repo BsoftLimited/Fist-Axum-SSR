1dd53cce119f709ae29b09efc339613e94bfd328
BsoftLimited <bsoftlimited@gmail.com>
2023-04-22 06:44:15 +0100

    finally done with tera integration

---
+++ assets/js/home.js
@@ -0,0 +1,41 @@
+document.addEventListener("alpine:init", ()=>{
+    Alpine.store(
+        "user", {
+            loading: false,
+            value: undefined,
+            login(){
+                this.value = true;
+                fetch("/api/user").then(response => response.json()).then(data =>{
+                    this.value = false;
+                    console.log(data);
+                    this.value = data;
+                });
+            },
+            logout(){
+                this.value = undefined;
+            },
+            signup(name, surname, email, password, repassword){
+                if(!name.length){
+                    alert("Name field cannot be empty");
+                }else if(!surname.length){
+                    alert("Surname field cannot be empty");
+                }else if(!email.length){
+                    alert("Email field cannot be empty");
+                }else if(!password.length){
+                    alert("Passowrd field cannot be empty");
+                }else if(password !== repassword){
+                    alert("Passowrd Mismatch");
+                }else{
+                    this.value = true;
+                    const details = { name: name,  surname: surname, email: email, password: password };
+                    const options = { method: 'POST', headers: { 'Content-Type': 'application/json', }, body: JSON.stringify(details), };
+                    fetch('/api/user/create', options).then(response => response.json()).then(data =>{
+                        this.value = false;
+                        console.log(data);
+                        this.value = data;
+                    });
+                }
+            }
+        }
+    );
+});
